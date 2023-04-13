9260606a78d498897869ca8afeb801c3bda4e529
Yulio Aldi Widargo <66007172+yulioaldiw@users.noreply.github.com>
2023-04-13 08:58:41 +0700

    project initialization

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	api := gin.Default()
+
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	api.Run(":8080")
+}
